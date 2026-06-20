5ddeb44fe0bbf82f3769918daf432f60336ff9d7
Gaston Baloira <gaston.baloira@outlook.com>
2026-06-20 12:49:21 +0200

    feat: ree_bronze_load.py (local validation)

--- 02-prerequisites/ree-api-bronze-load/ree_bronze_load.py
+++ 02-prerequisites/ree-api-bronze-load/ree_bronze_load.py
@@ -1,22 +1,20 @@
 """
-Sprint 1 — REE API Rest (Validacion Local).
-Generar Script para cargar datos desde la API de REE.
-Leer los datos y construir un DataFrame.
-Descargar los datos en formato Parquet en el directorio de trabajo.
+Sprint 1 — REE API Rest (Local validation).
+Generate a script to load data from the REE API.
+Read the data and build a DataFrame.
+Download the data in Parquet format to the working directory.
 """
 
-#Nativas (A-Z)
 from datetime import datetime, timezone
 import json
 import logging
 import time
 
-#PIP (A-Z)
 import pandas as pd
 import requests
 
 # ────────────────────────────────────────────────────────────────────────────
-# #TODO_0. Configurar Logging
+# Configure Logging
 # ────────────────────────────────────────────────────────────────────────────
 
 logging.basicConfig(
@@ -27,7 +25,7 @@ logging.basicConfig(
 log = logging.getLogger("api_ree_bronze_load")
 
 # ────────────────────────────────────────────────────────────────────────────
-# #TODO_1. Configurar variables de entorno para la API
+# Configure environment variables for the API
 # ────────────────────────────────────────────────────────────────────────────
 
 URL_BASE = "https://apidatos.ree.es"
@@ -36,14 +34,14 @@ START_YEAR = 2014
 END_YEAR = datetime.now().year
 TIME_TRUNC = "month"
 GEO_TRUNC = "electric_system"
-REGIONES = [
+REGIONS = [
     {"geo_name": "Espana", "geo_id": "8741", "geo_limit": "peninsular"},
     {"geo_name": "Madrid", "geo_id": "13", "geo_limit": "ccaa"},
     {"geo_name": "Catalunya", "geo_id": "9", "geo_limit": "ccaa"},
 ]
 
-TIMEOUT = 30 # segundos
-BASE = 2 # base para el backoff exponencial
+TIMEOUT = 30 
+BASE = 2 
 MAX_ATTEMPTS = 4 # attempts = 1 + 3 retries
 
 
@@ -52,18 +50,19 @@ PATH_OUTPUT = ("/Users/gastonbaloira/Projects/Portfolio/01_spi-indicators/"
                "02-prerequisites/ree-api-bronze-load/outputs/" + OUTPUT
 )
 # ────────────────────────────────────────────────────────────────────────────
-# #TODO_2. Crear funcion build_request_url
+# Create function build_request_url
 # ────────────────────────────────────────────────────────────────────────────
 
 def build_request_url(geo_limit: str, geo_ids: str) -> str:
     """
-    Construye la URL de la API de REE con las constantes 
-    y parámetros especificados.
+    Build the REE API URL with the constants and specified parameters.
     Args:
-        geo_limit (str): El valor del parámetro geo_limit para la URL.
-        geo_ids (str): El valor del parámetro geo_ids para la URL.    
+        geo_limit (str): The value of the geo_limit parameter for the URL.
+        geo_ids (str): The value of the geo_ids parameter for the URL.   
     Returns:
-        str: La URL completa para realizar la llamada a la API de REE."""
+        str: The complete URL to make the REE API call.
+    """
+    log.info("Building URL: ")
     url = (
         f"{URL_BASE}{ENDPOINT}?"
         f"start_date={START_YEAR}-01-01T00:00&"
@@ -73,124 +72,131 @@ def build_request_url(geo_limit: str, geo_ids: str) -> str:
         f"geo_limit={geo_limit}&"
         f"geo_ids={geo_ids}"
     )
-    
     return url
 
 # ────────────────────────────────────────────────────────────────────────────
-# #TODO_3. Crear funcion fetch_with_retry
+# Create function fetch_with_retry
 # ────────────────────────────────────────────────────────────────────────────
 
 def fetch_with_retry(url: str) -> dict:
-    """Realiza una llamada a la API con reintentos en caso de fallo.
+    """ 
+    Makes an API call with retries on failure.
     Args:
-        url (str): La URL de la API a la que se realizará la llamada.
+        url (str): The URL of the API to which the call will be made.
     Returns:
-        dict: La respuesta de la API en formato JSON si la llamada es exitosa.
+        dict: The API response in JSON format if the call is successful.
     Raises:
-        Exception: Si se alcanza el número máximo de reintentos sin éxito.
+        Exception: If the maximum number of unsuccessful retries is reached.
     """
-    
     for attempt in range(1, MAX_ATTEMPTS + 1):
         try:
-            log.info(f"Intento {attempt} de {MAX_ATTEMPTS}: Llamando a la API...")
+            log.info(f"Attempt {attempt} of {MAX_ATTEMPTS}: calling the API...")
             response = requests.get(url, timeout=TIMEOUT)
             status = response.status_code
 
-            #Exito: 200-299
+            #Success: 200-299
             if 200 <= status < 300:
-                log.info("Llamada a la API exitosa.")
+                log.info("Successful API call.")
                 return response.json()   
             
-            #Errores temporales
+            #Temporary errors
             elif status in (429, 500, 502, 503, 504):
                 if attempt < MAX_ATTEMPTS:
                     delay = BASE ** attempt
-                    log.warning(f"Error temporal {status}. Reintentando...")
+                    log.warning(f"Temporary error {status}. Retrying...")
                     time.sleep(delay)
                 else:
-                    log.error(f"Máximo de reintentos alcanzado. Error temporal persistente: {status}.")
-                    raise Exception(f"Error temporal persistente en la API: {status}")
+                    log.error(f"Maximum number of retries reached. Persistent temporary error: {status}.")
+                    raise Exception(f"Persistent temporary error in the API: {status}")
             
-            #Errores permanentes
+            #Permanent errors
             else:
-                log.error(f"Error inesperado {status}. No se reintentará.")
-                raise Exception(f"Error inesperado en la API: {status}")
+                log.error(f"Unexpected error {status}. No retry.")
+                raise Exception(f"Unexpected error in the API: {status}")
         
         except (requests.exceptions.RequestException):
             if attempt < MAX_ATTEMPTS:
                 delay = BASE ** attempt
-                log.info(f"Reintentando en {delay} segundos...")
+                log.info(f"Retrying in {delay} seconds...")
                 time.sleep(delay)
             
             else:
-                log.error("Máximo de reintentos alcanzado. Fallo la llamada a la API.")
+                log.error("Maximum retries reached. API call failed..")
                 raise
 
-    raise RuntimeError("Error al llamar a la API después de varios intentos.")
+    raise RuntimeError("Error calling the API after several attempts.")
 
 # ────────────────────────────────────────────────────────────────────────────
-# #TODO_4. Crear funcion parse_response
+# Create function parse_response
 # ────────────────────────────────────────────────────────────────────────────
 
 def parse_response(response: dict, geo_name: str) -> list:
     """
-    Parsea la respuesta de la API y extrae los datos relevantes.
+    Parse the API response and extract the relevant data.
     Args:
-        response (dict): La respuesta de la API en formato JSON.
-        geo_name (str): El nombre de la geolocalización.
+        response (dict): The API response in JSON format.
+        geo_name (str): The name of the geolocation.
     Returns:
-        list: Una lista de diccionarios con los datos parseados.
+        list: A list of dictionaries with the parsed data.
     """
-    filas = []
-    for serie in response["included"]:  
-        serie_type = serie["type"] 
-        for point in serie["attributes"]["values"]:
+    log.info("Parsing API response..")
+    rows = []
+    for series in response["included"]:  
+        series_type = series["type"] 
+        for point in series["attributes"]["values"]:
             fila = {
-                "serie_type": serie_type,
+                "serie_type": series_type,
                 "geo_name": geo_name,
                 "datetime": point.get("datetime"),
                 "value": point.get("value"),
                 "percentage": point.get("percentage")
             }
-            filas.append(fila)
-
-    return filas
+            rows.append(fila)
+    return rows
 
 # ────────────────────────────────────────────────────────────────────────────
-# #TODO_5. Crear funcion build_dataframe
+# Create function build_dataframe
 # ────────────────────────────────────────────────────────────────────────────
 
 def build_dataframe(data: list) -> pd.DataFrame:
     """
-    Construye un DataFrame de pandas a partir de una lista de diccionarios.
+    Builds a pandas DataFrame from a list of dictionaries.
     Args:
-        data (list): Una lista de diccionarios con los datos parseados.
+        data (list): A list of dictionaries with the parsed data.
     Returns:
-        pd.DataFrame: Un DataFrame con los datos.
+        pd.DataFrame: A DataFrame with the data.
     """
+    log.info("Building DataFrame..")
     df = pd.DataFrame(data)
     df["_ingestion_timestamp"] = datetime.now(timezone.utc).isoformat()
     df["_source_api"] = ENDPOINT
-    df = df.astype(str) # Se convierte a string por principio Bronze.
-
+    df = df.astype(str) # Converted to String by the principle of bronze layer 
     return df
 
 # ────────────────────────────────────────────────────────────────────────────
-# #TODO_6. Crear main para ejecutar el proceso completo
+# Create main to run the entire process
 # ────────────────────────────────────────────────────────────────────────────
 
-#if __name__ == "__main__":
-    # Construir la URL de la API
-    # url = build_request_url(URL_BASE, ENDPOINT, GEO_1, GEO_2, GEO_3)
+def main() -> None:
+    """
+    Main function that executes the entire process Data loading from the REE API.
+    """
+    all_data = []
+    for region in REGIONS:
+        geo_name = region["geo_name"]
+        geo_id = region["geo_id"]
+        geo_limit = region["geo_limit"]
+
+        url = build_request_url(geo_limit, geo_id)
+        log.info(f"Processing region: {geo_name}")
+
+        response = fetch_with_retry(url)
+        parsed_data = parse_response(response, geo_name)
+        all_data.extend(parsed_data)
+
+    df = build_dataframe(all_data)
+    df.to_parquet(PATH_OUTPUT, index=False)
+    log.info(f"Data stored in {PATH_OUTPUT}")
     
-    # Realizar la llamada a la API con reintentos
-    # response = fetch_with_retry(url, TIMEOUT, RETRIES, RETRY_DELAY)
-    
-    # Parsear la respuesta de la API
-    # data = parse_response(response)
-    
-    # Construir el DataFrame
-    # df = build_dataframe(data)
-    
-    # Guardar el DataFrame en formato Parquet
-    # df.to_parquet(PATH_OUTPUT, index=False)
+if __name__ == "__main__":
+    main()
